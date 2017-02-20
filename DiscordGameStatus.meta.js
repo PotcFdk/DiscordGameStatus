1069b9517ea34d87517a3bb7ac7807f1c1f63633
PotcFdk <PotcFdk@gmx.net>
2017-02-20 16:52:04 +0100

    Add initial files.

---
+++ DiscordGameStatus.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @author      PotcFdk
+// @namespace   https://github.com/PotcFdk
+// @name        DiscordGameStatus
+// @description A userscript for setting the currently playing game in the Discord web client
+// @include     https://discordapp.com/*
+// @version     1.0.0
+// @grant       GM_unsafeWindow
+// @run-at      document-start
+// @downloadURL https://raw.githubusercontent.com/PotcFdk/DiscordGameStatus/master/DiscordGameStatus.user.js
+// @updateURL   https://raw.githubusercontent.com/PotcFdk/DiscordGameStatus/master/DiscordGameStatus.meta.js
+// ==/UserScript==
