b0729bbfa0ac996791d715d67bc4d906f6e30e5e
PotcFdk <PotcFdk@gmx.net>
2017-04-11 20:03:28 +0200

    This is version 1.2.0.

--- DiscordGameStatus.user.js
+++ DiscordGameStatus.user.js
@@ -4,7 +4,7 @@
 // @name        DiscordGameStatus
 // @description A userscript for setting the currently playing game in the Discord web client
 // @include     https://discordapp.com/*
-// @version     1.1.1
+// @version     1.2.0
 // @grant       GM_unsafeWindow
 // @run-at      document-start
 // @downloadURL https://raw.githubusercontent.com/PotcFdk/DiscordGameStatus/master/DiscordGameStatus.user.js
