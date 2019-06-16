62cb41b4996caf1ed4c1c435e0844e14ca414dd1
Kevin Jiang <kevin51jiang@email.com>
2019-06-16 01:34:09 -0400

    Update DiscordGameStatus.user.js

--- DiscordGameStatus.user.js
+++ DiscordGameStatus.user.js
@@ -4,7 +4,7 @@
 // @name        DiscordGameStatus
 // @description A userscript for setting the currently playing game in the Discord web client
 // @include     https://discordapp.com/*
-// @version     1.8
+// @version     1.8.0
 // @grant       GM_unsafeWindow
 // @run-at      document-end
 // @downloadURL https://raw.githubusercontent.com/PotcFdk/DiscordGameStatus/master/DiscordGameStatus.user.js
